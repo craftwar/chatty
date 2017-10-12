8435be7c33b506115a2489b9a57f0edbdf60569e
craftwar <craftwar@users.noreply.github.com>
2017-10-12 11:18:10 +0800

    Create Chatty portable.cmd
    To run Chatty in portable mode, put this file in Chatty root directory(same as Chatty.jar) and run it.

---
+++ Chatty portable.cmd
@@ -0,0 +1,3 @@
+start /D "%cd%" javaw -jar Chatty.jar -cd
+@REM sample arguments to javaw (you should tweak to fit your needs)
+:: start /D "%cd%" javaw -XX:+AggressiveOpts -da -Xbatch -Xmx200M -jar Chatty.jar -cd 
